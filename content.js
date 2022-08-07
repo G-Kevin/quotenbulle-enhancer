eb16bf65eaea958698b436b881df3f9b0df9dbb7
G-Kevin <G-Kevin@users.noreply.github.com>
2022-08-07 18:00:17 +0200

    added copy2Excel functionality for Kombirechner

--- content.js
+++ content.js
@@ -27,7 +27,8 @@ function createCopyToExcelButton() {
 
         // compile out visible copy-Button
         var $copyButton = $('<div style="margin: 0 auto; text-align: center"><a class="qb_button" href="javascript:createAndCopyExcelStringToClipboard()" style="text-decoration:none !important; font-size:1.3em !important;">Werte für Excel kopieren</a></div>');
-        $copyButton.appendTo($("#pgc-328-1-0"));
+        $copyButton.appendTo($("#pgc-328-1-0")); // Bullenrechner
+        $copyButton.appendTo($("#pgc-47791-1-0")); // Kombirechner
     } catch (e) {
         console.error("ERROR in QB copyToExcel: ", e.message);
     }
@@ -51,44 +52,22 @@ function copyStringToClipboard(str) {
 }
 
 function createAndCopyExcelStringToClipboard(orbitLandingDomain) {
-    try {
-        var date = document.getElementById("calc_date").innerHTML.replace(/\s-\s.*/g, '').replace(/(\d{2})\.(\d{2})\.(\d{2})/g, '$1.$2.20$3');
-    } catch {
-        var date = "";
-    }
+    url = window.location.pathname;
+    var status = "Offen";
+    var excelString = "";
     var category = "Pre-Game";
     var sport = "Fußball";
     var league = "";
-    try {
-        var event = document.getElementById("calc_event").getElementsByTagName('strong')[0].innerHTML;
-    } catch {
-        var event = "";
-    }
-    var betType = "1X2";
-
-    try {
-        var ergebnis = document.getElementById("calc_outcome").innerHTML;
-        var mannschaft1 = event.replace(/\s-\s.*/g, '');
-        var tipBack = ergebnis === mannschaft1 ? "1" : ergebnis === "Unentschieden" ? "X" : "2";
-        var tipLay = "lay" + tipBack;
-    } catch {
-        var tipBack = "";
-        var tipLay = "";
-    }
-
-    var dropdownListBetMode = document.getElementById("bet_mode");
-    var selectedDropdownListBetMode = dropdownListBetMode.options[dropdownListBetMode.selectedIndex].text;
-
-    var stakeBack = selectedDropdownListBetMode.includes("Freiwette") ? ("=" + document.getElementById("stake_bookie").value) + "-" + document.getElementById("stake_bookie").value : document.getElementById("stake_bookie").value;
-    var stakeLay = document.getElementById("liability_layer").value;
-    var oddBack = document.getElementById("odds_bookie").value;
-    var oddLay = document.getElementById("odds_layer").value;
-    oddLay = "=" + oddLay + "/(" + oddLay + "-1)";
-
-    var dropdownListCommissionBack = document.getElementById("tax_germany");
-    var comissionOnWinningsBack = dropdownListCommissionBack.options[dropdownListCommissionBack.selectedIndex].text.includes("vom Nettogewinn") ? "j" : "";
-
+    var bookie = "";
     var comissionOnWinningsLay = orbitCommissionOnWinnings;
+
+    if (url === "/tools/rechner/") {
+        var dropdownListCommissionBack = document.getElementById("tax_germany");
+    } else {
+        var dropdownListCommissionBack = document.getElementById("select_tax_germany");
+    }
+
+    var comissionOnWinningsBack = dropdownListCommissionBack.options[dropdownListCommissionBack.selectedIndex].text.includes("vom Nettogewinn") ? "j" : "";
 
     switch (dropdownListCommissionBack.options[dropdownListCommissionBack.selectedIndex].value) {
       case "no_tax":
@@ -115,46 +94,189 @@ function createAndCopyExcelStringToClipboard(orbitLandingDomain) {
       default:
         throw "Unknown commission type for back!"
     }
-
     commissionBack = translateCommissionForExcel(commissionBack);
     var commissionLay = translateCommissionForExcel(orbitCommission);
 
-    var bookie = "";
-    var status = "Offen";
+    if (url === "/tools/rechner/") {
+        // handle Bullenrechner
+        try {
+            var date = document.getElementById("calc_date").innerHTML.replace(/\s-\s.*/g, '').replace(/(\d{2})\.(\d{2})\.(\d{2})/g, '$1.$2.20$3');
+        } catch {
+            var date = "";
+        }
 
-    var excelString = "";
-    // compile clipboard string
-    // first line, back
-    excelString += addFragmentToExcelString(date);
-    excelString += addFragmentToExcelString("");
-    excelString += addFragmentToExcelString(category);
-    excelString += addFragmentToExcelString(sport);
-    excelString += addFragmentToExcelString(league);
-    excelString += addFragmentToExcelString(event);
-    excelString += addFragmentToExcelString(betType);
-    excelString += addFragmentToExcelString(tipBack);
-    excelString += addFragmentToExcelString(stakeBack);
-    excelString += addFragmentToExcelString(oddBack);
-    excelString += addFragmentToExcelString(comissionOnWinningsBack ? "j" : "");
-    excelString += addFragmentToExcelString(commissionBack);
-    excelString += addFragmentToExcelString(bookie);
-    excelString += addFragmentToExcelString(status, false, true);
+        try {
+            var event = document.getElementById("calc_event").getElementsByTagName('strong')[0].innerHTML;
+        } catch {
+            var event = "";
+        }
+        var betType = "1X2";
 
-    // second line, lay
-    excelString += addFragmentToExcelString(date);
-    excelString += addFragmentToExcelString("");
-    excelString += addFragmentToExcelString(category);
-    excelString += addFragmentToExcelString(sport);
-    excelString += addFragmentToExcelString(league);
-    excelString += addFragmentToExcelString(event);
-    excelString += addFragmentToExcelString(betType);
-    excelString += addFragmentToExcelString(tipLay);
-    excelString += addFragmentToExcelString(stakeLay);
-    excelString += addFragmentToExcelString(oddLay);
-    excelString += addFragmentToExcelString(comissionOnWinningsLay ? "j" : "");
-    excelString += addFragmentToExcelString(commissionLay);
-    excelString += addFragmentToExcelString(orbitNameInExcel);
-    excelString += addFragmentToExcelString(status, false);
+        try {
+            var ergebnis = document.getElementById("calc_outcome").innerHTML;
+            var mannschaft1 = event.replace(/\s-\s.*/g, '');
+            var tipBack = ergebnis === mannschaft1 ? "1" : ergebnis === "Unentschieden" ? "X" : "2";
+            var tipLay = "lay" + tipBack;
+        } catch {
+            var tipBack = "";
+            var tipLay = "";
+        }
+
+        var dropdownListBetMode = document.getElementById("bet_mode");
+        var selectedDropdownListBetMode = dropdownListBetMode.options[dropdownListBetMode.selectedIndex].text;
+
+        var stakeBack = selectedDropdownListBetMode.includes("Freiwette") ? ("=" + document.getElementById("stake_bookie").value) + "-" + document.getElementById("stake_bookie").value : document.getElementById("stake_bookie").value;
+        var stakeLay = document.getElementById("liability_layer").value;
+        var oddBack = document.getElementById("odds_bookie").value;
+        var oddLay = document.getElementById("odds_layer").value;
+        oddLay = "=" + oddLay + "/(" + oddLay + "-1)";
+
+        // first line, back
+        excelString += addFragmentToExcelString(date);
+        excelString += addFragmentToExcelString("");
+        excelString += addFragmentToExcelString(category);
+        excelString += addFragmentToExcelString(sport);
+        excelString += addFragmentToExcelString(league);
+        excelString += addFragmentToExcelString(event);
+        excelString += addFragmentToExcelString(betType);
+        excelString += addFragmentToExcelString(tipBack);
+        excelString += addFragmentToExcelString(stakeBack);
+        excelString += addFragmentToExcelString(oddBack);
+        excelString += addFragmentToExcelString(comissionOnWinningsBack ? "j" : "");
+        excelString += addFragmentToExcelString(commissionBack);
+        excelString += addFragmentToExcelString(bookie);
+        excelString += addFragmentToExcelString(status, false, true);
+
+        // second line, lay
+        excelString += addFragmentToExcelString(date);
+        excelString += addFragmentToExcelString("");
+        excelString += addFragmentToExcelString(category);
+        excelString += addFragmentToExcelString(sport);
+        excelString += addFragmentToExcelString(league);
+        excelString += addFragmentToExcelString(event);
+        excelString += addFragmentToExcelString(betType);
+        excelString += addFragmentToExcelString(tipLay);
+        excelString += addFragmentToExcelString(stakeLay);
+        excelString += addFragmentToExcelString(oddLay);
+        excelString += addFragmentToExcelString(comissionOnWinningsLay ? "j" : "");
+        excelString += addFragmentToExcelString(commissionLay);
+        excelString += addFragmentToExcelString(orbitNameInExcel);
+        excelString += addFragmentToExcelString(status, false);
+    } else if (url === "/tools/kombirechner/") {
+        // handle Kombirechner
+        // 
+        // Kombirechner is able to handle 2-5 bets
+        // No need to test for count of rows, just test if an odd is empty
+        // fill an array in reverse order so we can use the information from the latest line
+        var latestDate = "";
+        var excelLines = [];
+        var isLatestLine = true;
+
+        var event = "";
+        var tipBack = "";
+        var oddBack = "";
+
+        var betType = "Kombi";
+        var dropdownListBetMode = document.getElementById("select_acca_bet_mode");
+        var selectedDropdownListBetMode = dropdownListBetMode.options[dropdownListBetMode.selectedIndex].text;
+
+        stakeBack = selectedDropdownListBetMode.includes("Freiwette") ? ("=" + document.getElementById("input_back_stake").value) + "-" + document.getElementById("input_back_stake").value : document.getElementById("input_back_stake").value;
+
+        for (let i = 5; i > 0; i--) {
+            excelLines[i] = {};
+            if (isLatestLine && document.getElementById('input_back_odds_' + i.toString()).value !== '') {
+                isLatestLine = false; // block upcoming iterations
+                latestDate = document.getElementById('input_date_' + i.toString()).value.replace(/\s-\s.*/g, '').replace(/(\d{2})\.(\d{2})\.(\d{2})/g, '$1.$2.20$3');
+            }
+
+            // collect general information from this line
+            if (document.getElementById('input_back_odds_' + i.toString()).value !== '') {
+                try {
+                    excelLines[i].date = document.getElementById('input_date_' + i.toString()).value.replace(/\s-\s.*/g, '').replace(/(\d{2})\.(\d{2})\.(\d{2})/g, '$1.$2.20$3');
+                } catch {
+                    excelLines[i].date = "";
+                }
+
+                try {
+                    excelLines[i].event = document.getElementById("input_event_" + i.toString()).value;
+                } catch {
+                    excelLines[i].event = "";
+                }
+
+                try {
+                    var ergebnis = document.getElementById("input_outcome_" + i.toString()).value;
+                    var mannschaft1 = excelLines[i].event.replace(/\s-\s.*/g, '');
+                    excelLines[i].tipBack = ergebnis === mannschaft1 ? "1" : ergebnis === "Unentschieden" ? "X" : "2";
+                    excelLines[i].tipLay = "lay" + excelLines[i].tipBack;
+                } catch {
+                    excelLines[i].tipBack = "";
+                    excelLines[i].tipLay = "";
+                }
+
+                excelLines[i].stakeLay = document.getElementById("input_liability_" + i.toString()).value.replace(/\./g, ",");
+
+                excelLines[i].oddBack = document.getElementById("input_back_odds_" + i.toString()).value;
+                excelLines[i].oddLay = document.getElementById("input_lay_odds_" + i.toString()).value;
+                excelLines[i].oddLay = "=" + excelLines[i].oddLay + "/(" + excelLines[i].oddLay + "-1)";
+
+                if (event === "") {
+                    event = excelLines[i].event;
+                } else {
+                    event = excelLines[i].event + " + " + event;
+                }
+
+                if (tipBack === "") {
+                    tipBack = excelLines[i].tipBack;
+                } else {
+                    tipBack = excelLines[i].tipBack + " + " + tipBack;
+                }
+
+                if (oddBack === "") {
+                    oddBack = "=" + excelLines[i].oddBack;
+                } else {
+                    oddBack += "*" + excelLines[i].oddBack;
+                }
+            }
+        }
+
+
+        // first line, back
+        excelString += addFragmentToExcelString(latestDate);
+        excelString += addFragmentToExcelString("");
+        excelString += addFragmentToExcelString(category);
+        excelString += addFragmentToExcelString(sport);
+        excelString += addFragmentToExcelString(league);
+        excelString += addFragmentToExcelString(event);
+        excelString += addFragmentToExcelString(betType);
+        excelString += addFragmentToExcelString(tipBack);
+        excelString += addFragmentToExcelString(stakeBack);
+        excelString += addFragmentToExcelString(oddBack);
+        excelString += addFragmentToExcelString(comissionOnWinningsBack ? "j" : "");
+        excelString += addFragmentToExcelString(commissionBack);
+        excelString += addFragmentToExcelString(bookie);
+        excelString += addFragmentToExcelString(status, false, true);
+
+        for (let i = 1; i < 6; i++) {
+            // lay: all other lines
+            
+            if (excelLines[i].oddLay !== undefined) {
+                excelString += addFragmentToExcelString(excelLines[i].date);
+                excelString += addFragmentToExcelString("");
+                excelString += addFragmentToExcelString(category);
+                excelString += addFragmentToExcelString(sport);
+                excelString += addFragmentToExcelString(league);
+                excelString += addFragmentToExcelString(excelLines[i].event);
+                excelString += addFragmentToExcelString(betType);
+                excelString += addFragmentToExcelString(excelLines[i].tipLay);
+                excelString += addFragmentToExcelString(excelLines[i].stakeLay);
+                excelString += addFragmentToExcelString(excelLines[i].oddLay);
+                excelString += addFragmentToExcelString(comissionOnWinningsLay ? "j" : "");
+                excelString += addFragmentToExcelString(commissionLay);
+                excelString += addFragmentToExcelString(orbitNameInExcel);
+                excelString += addFragmentToExcelString(status, false, true);
+            }
+        }
+    }
 
     copyStringToClipboard(excelString);
 }
@@ -213,11 +335,9 @@ setTimeout(() => {
         if (window.location.pathname === "/tools/kombirechner/") {
             // set listener on input field to trigger formatter after every change
             $("input[id^='input_back_odds_']").on("change paste keyup propertychange input", function() {
-                // console.info("event listener triggered");
                 formatLayStakeLiabilityValues(orbitLandingDomain);
             });
             $("input[id^='input_lay_odds_']").on("change paste keyup propertychange input", function() {
-                // console.info("event listener triggered");
                 formatLayStakeLiabilityValues(orbitLandingDomain);
             });
 
